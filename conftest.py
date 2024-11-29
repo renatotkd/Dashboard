f00bf734cd52b2f7ff7d0c73631e0f48725f68fe
Joao Renato <renatotkd55@gmail.com>
2024-11-29 11:53:20 -0300

    alteracoes para teste

--- conftest.py
+++ conftest.py
@@ -4,9 +4,17 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
-    chrome_path = "/home/renato/programas/dash/chrome-linux64/chrome"
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
+
+
+#    chrome_path = "/home/renato/programas/dash/chrome-linux64/chrome"
+#    chrome_options = webdriver.ChromeOptions()
+#    chrome_options.binary_location = chrome_path
+#    driver = webdriver.Chrome(options=chrome_options)
+#    yield driver
+#    driver.quit()
--- teste/test_home.py
+++ teste/test_home.py
@@ -1,9 +1,9 @@
 import time
 
 def test_home(browser):
-    url="http:127.0.0.1:8080"
+    url="http:127.0.0.1:8081"
     browser.get(url)
-    time.sleep(10)
+    time.sleep(5)
 
     assert browser.title == "Dash"
     print('teste pagina inicial')
